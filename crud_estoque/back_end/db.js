b29420f6b83a3648b797b8d66a15c0f7e1ead52f
Jéssica Sousa <jskoliver@hotmail.com>
2023-10-13 11:42:49 -0300

    "refactor: Change in the name of the product table"

--- crud_estoque/back_end/db.js
+++ crud_estoque/back_end/db.js
@@ -14,33 +14,33 @@ connect();
 
 async function SelectProducts() {
   const client = await connect();
-  const res = await client.query("SELECT * FROM produto");
+  const res = await client.query("SELECT * FROM cadastro_produto");
   return res.rows;
 }
 
 async function SelectProduct(codigo) {
   const client = await connect();
-  const res = await client.query("SELECT * FROM produto WHERE CODIGO=$1", [codigo]);
+  const res = await client.query("SELECT * FROM cadastro_produto WHERE CODIGO=$1", [codigo]);
   return res.rows;
 }
 
 async function InsertProduct(product) {
   const client = await connect();
-  const sql = "INSERT INTO produto(produto,valor,codigo) VALUES ($1,$2,$3);";
+  const sql = "INSERT INTO cadastro_produto(produto,valor,codigo) VALUES ($1,$2,$3);";
   const values = [product.produto, product.valor, product.codigo]
   await client.query(sql, values);
 }
 
 async function UpDateProduct(codigo, product) {
   const client = await connect();
-  const sql = "UPDATE produto SET produto=$1, valor=$2 WHERE codigo=$3";
+  const sql = "UPDATE cadastro_produto SET produto=$1, valor=$2 WHERE codigo=$3";
   const values = [product.produto, product.valor, codigo]
   await client.query(sql, values);
 }
 
 async function DeleteProduct(codigo) {
   const client = await connect();
-  const sql = "DELETE FROM produto WHERE codigo=$1";
+  const sql = "DELETE FROM cadastro_produto WHERE codigo=$1";
   return await client.query(sql, [codigo]);
 }
 
